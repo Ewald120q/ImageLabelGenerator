ed90f380d6757d07202538fe98cd23b19663c57d
Ewald120q <ewald.oleksjuk@udo.edu>
2023-04-29 13:47:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from PIL import Image
+def readData():
+
+
+def createDatabase():
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Hi')
+    #daten einlesen
+    #create_database()
+    #label_picture()
+    #fill_label_in_database()
+    #return database as csv
