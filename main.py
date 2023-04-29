36596b61717756a7ff093c3da9d32ede5393a723
Ewald <ewald.oleksjuk@tu.dortmund.de>
2023-04-29 17:53:13 +0200

    first update

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from PIL import Image
-def readData():
+import pandas as pd
+import numpy as np
+import os
+import glob
+
+def checkImage(image: Image):
+    for x in range(0,image.width):
+        for y in range(0,image.height):
+            print('abx')
 
 
-def createDatabase():
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('Hi')
-    #daten einlesen
-    #create_database()
-    #label_picture()
-    #fill_label_in_database()
-    #return database as csv
+    #read color that we want to search
+    rightColor = Image.open('right_color.jpg').getpixel((0,0))
+    print(rightColor)
+
+    #get our images that we want to scan
+    data = os.listdir(glob.glob('./seed**/')[0])
+
+    #create database that gets filled with names, which afterwards get associated with labels
+    dataframe = pd.DataFrame(data, columns=['Name'])
+
+    labels = []
+    for image in data:
+        labels.append(checkImage(image))
+
+
+
+
+    # label_picture()
+    # fill_label_in_database()
+    # return database as csv
