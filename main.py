9bacf6d701c2e9d7c858c727ac366d678e58232d
Ewald <ewald.oleksjuk@tu.dortmund.de>
2023-04-30 00:41:42 +0200

    changed from Pillow to Matplotlib+Numpy

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
-from PIL import Image
 import pandas as pd
 import numpy as np
 import os
 import glob
+import matplotlib.image as imread
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+from PIL import Image
 
-def checkRange(image: Image, length, min): #checks, if right
 
+def checkImage(image, id,radius=2):
+    #print(image)
+    img = np.array(image)
+    r = 0
+    g = 1
+    b = 2
 
-def checkImage(image: Image):
-    for x in range(0,image.width):
-        for y in range(0,image.height):
-            if image.getpixel((x,y))==rightColor:
-                print('a')
+    r_query = 250/255
+    g_query = 170/255
+    b_query = 31/255
+    y, x = (np.where((img[:, :, r] >= r_query) & (img[:, :, g] >= g_query) & (img[:, :, b] <= b_query)))
+    if len(y)>1:
+        plt.imshow(image)
+        plt.ylabel(id)
+        plt.show()
 
 
 if __name__ == '__main__':
     #read color that we want to search
-    rightColor = Image.open('right_color.jpg').getpixel((0,0))
+    rightColor = imread.imread('right_color.jpg')[0][0]
     print(rightColor)
 
     #get our images that we want to scan
     data = os.listdir(glob.glob('./seed**/')[0])
+    print(data)
 
     #create database that gets filled with names, which afterwards get associated with labels
     dataframe = pd.DataFrame(data, columns=['Name'])
 
+    print(glob.glob('./seed**/')[0],data[0])
+    print(data[0])
+    #Image.open(glob.glob('./seed**/')[0]+data[0]).show()
+
+
     labels = []
     for image in data:
-        labels.append(checkImage(image))
+        print(image)
+        labels.append(checkImage(imread.imread(glob.glob('./seed**/')[0]+image), image))
+
 
 
 
